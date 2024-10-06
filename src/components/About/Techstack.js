26f8219877918fa26ec4a2cb71a9119a9fdde346
VisuallyImpairedMap <visuallyimpairedmap@gmail.com>
2024-10-05 23:54:15 -0300

    third  commit

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,23 +1,6 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
-import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiPython,
-  DiGit,
-  DiJava,
-} from "react-icons/di";
-import {
-  SiRedis,
-  SiFirebase,
-  SiNextdotjs,
-  SiSolidity,
-  SiPostgresql,
-} from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+import { Row } from "react-bootstrap";
+
 
 function Techstack() {
   return (
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, } from "react-bootstrap";
-import Particle from "../Particle";
+
 
 
 function Projects() {
